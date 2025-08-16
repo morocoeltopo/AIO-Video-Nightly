c9295f94f1908e0579c70c4fa1f28a8fdbdbab39
Shiba Prasad <shiba.spj@hotmail.com>
2025-08-16 10:01:05 +0530

    Optimize performance of data read/write operations
    Reduced overhead in serialization and storage to improve overall
    application responsiveness.
    
    part -8

--- app/src/main/java/app/ui/main/fragments/browser/activities/BookmarksActivity.kt
+++ app/src/main/java/app/ui/main/fragments/browser/activities/BookmarksActivity.kt
@@ -23,174 +23,172 @@ import lib.ui.builders.ToastView.Companion.showToast
 import java.lang.ref.WeakReference
 
 class BookmarksActivity : BaseActivity(),
-    AIOTimerListener, OnBookmarkItemClick, OnBookmarkItemLongClick {
+	AIOTimerListener, OnBookmarkItemClick, OnBookmarkItemLongClick {
 
-    private val safeSelfReference = WeakReference(this)
-    private val safeBookmarksActivityRef = safeSelfReference.get()
+	private val safeSelfReference = WeakReference(this)
+	private val safeBookmarksActivityRef = safeSelfReference.get()
 
-    private lateinit var emptyBookmarksIndicator: View
-    private lateinit var bookmarkListView: ListView
-    private lateinit var buttonLoadMoreBookmarks: View
+	private lateinit var emptyBookmarksIndicator: View
+	private lateinit var bookmarkListView: ListView
+	private lateinit var buttonLoadMoreBookmarks: View
 
-    private var bookmarkOptionPopup: BookmarkOptionPopup? = null
+	private var bookmarkOptionPopup: BookmarkOptionPopup? = null
 
-    private val bookmarksAdapter by lazy {
-        val activityRef = safeBookmarksActivityRef
-        BookmarkAdapter(activityRef, activityRef, activityRef)
-    }
+	private lateinit var bookmarksAdapter: BookmarkAdapter
 
-    override fun onRenderingLayout(): Int {
-        return R.layout.activity_bookmarks_1
-    }
+	override fun onRenderingLayout(): Int {
+		return R.layout.activity_bookmarks_1
+	}
 
-    override fun onAfterLayoutRender() {
-        initializeViews()
-        initializeViewsOnClickEvents()
-    }
+	override fun onAfterLayoutRender() {
+		initializeViews()
+		initializeViewsOnClickEvents()
+	}
 
-    override fun onResumeActivity() {
-        super.onResumeActivity()
-        registerToAIOTimer()
-        updateBookmarkListAdapter()
-    }
+	override fun onResumeActivity() {
+		super.onResumeActivity()
+		registerToAIOTimer()
+		updateBookmarkListAdapter()
+	}
 
-    override fun onPauseActivity() {
-        super.onPauseActivity()
-        unregisterToAIOTimer()
-    }
+	override fun onPauseActivity() {
+		super.onPauseActivity()
+		unregisterToAIOTimer()
+	}
 
-    override fun onBackPressActivity() {
-        closeActivityWithFadeAnimation(false)
-    }
+	override fun onBackPressActivity() {
+		closeActivityWithFadeAnimation(false)
+	}
 
-    override fun onDestroy() {
-        unregisterToAIOTimer()
-        bookmarkListView.adapter = null
-        bookmarkOptionPopup?.close()
-        bookmarkOptionPopup = null
-        super.onDestroy()
-    }
+	override fun onDestroy() {
+		unregisterToAIOTimer()
+		bookmarkListView.adapter = null
+		bookmarkOptionPopup?.close()
+		bookmarkOptionPopup = null
+		super.onDestroy()
+	}
 
-    override fun onAIOTimerTick(loopCount: Double) {
-        updateLoadMoreButtonVisibility()
-    }
+	override fun onAIOTimerTick(loopCount: Double) {
+		updateLoadMoreButtonVisibility()
+	}
 
-    override fun onBookmarkClick(bookmarkModel: BookmarkModel) {
-        openBookmarkInBrowser(bookmarkModel)
-    }
+	override fun onBookmarkClick(bookmarkModel: BookmarkModel) {
+		openBookmarkInBrowser(bookmarkModel)
+	}
 
-    override fun onBookmarkLongClick(
-        bookmarkModel: BookmarkModel,
-        position: Int, listView: View
-    ) {
-        safeBookmarksActivityRef?.let { safeBookmarkActivityRef ->
-            try {
-                bookmarkOptionPopup = null
-                bookmarkOptionPopup = BookmarkOptionPopup(
-                    bookmarksActivity = safeBookmarkActivityRef,
-                    bookmarkModel = bookmarkModel,
-                    listView = listView
-                ).apply { show() }
-            } catch (error: Exception) {
-                error.printStackTrace()
-                showToast(msgId = R.string.text_something_went_wrong)
-            }
-        }
-    }
+	override fun onBookmarkLongClick(
+		bookmarkModel: BookmarkModel,
+		position: Int, listView: View
+	) {
+		safeBookmarksActivityRef?.let { safeBookmarkActivityRef ->
+			try {
+				bookmarkOptionPopup = null
+				bookmarkOptionPopup = BookmarkOptionPopup(
+					bookmarksActivity = safeBookmarkActivityRef,
+					bookmarkModel = bookmarkModel,
+					listView = listView
+				).apply { show() }
+			} catch (error: Exception) {
+				error.printStackTrace()
+				showToast(msgId = R.string.text_something_went_wrong)
+			}
+		}
+	}
 
-    override fun clearWeakActivityReference() {
-        safeSelfReference.clear()
-        super.clearWeakActivityReference()
-    }
+	override fun clearWeakActivityReference() {
+		safeSelfReference.clear()
+		super.clearWeakActivityReference()
+	}
 
-    private fun initializeViews() {
-        safeBookmarksActivityRef?.let {
-            emptyBookmarksIndicator = findViewById(R.id.empty_bookmarks_indicator)
-            bookmarkListView = findViewById(R.id.list_bookmarks)
-            buttonLoadMoreBookmarks = findViewById(R.id.btn_load_more_bookmarks)
+	private fun initializeViews() {
+		safeBookmarksActivityRef?.let {
+			emptyBookmarksIndicator = findViewById(R.id.empty_bookmarks_indicator)
+			bookmarkListView = findViewById(R.id.list_bookmarks)
+			buttonLoadMoreBookmarks = findViewById(R.id.btn_load_more_bookmarks)
 
-            bookmarkListView.adapter = bookmarksAdapter
-            bookmarkListView.visibility = GONE
+			bookmarksAdapter = BookmarkAdapter(it, it, it)
+			bookmarkListView.adapter = bookmarksAdapter
+			bookmarkListView.visibility = GONE
 
-            emptyBookmarksIndicator.visibility = GONE
-            buttonLoadMoreBookmarks.visibility = GONE
-            updateLoadMoreButtonVisibility()
-        }
-    }
+			emptyBookmarksIndicator.visibility = GONE
+			buttonLoadMoreBookmarks.visibility = GONE
+			updateLoadMoreButtonVisibility()
+		}
+	}
 
-    private fun initializeViewsOnClickEvents() {
-        findViewById<View>(R.id.btn_left_actionbar)
-            .setOnClickListener { onBackPressActivity() }
+	private fun initializeViewsOnClickEvents() {
+		findViewById<View>(R.id.btn_left_actionbar)
+			.setOnClickListener { onBackPressActivity() }
 
-        findViewById<View>(R.id.btn_right_actionbar)
-            .setOnClickListener { deleteAllBookmarks() }
+		findViewById<View>(R.id.btn_right_actionbar)
+			.setOnClickListener { deleteAllBookmarks() }
 
-        buttonLoadMoreBookmarks.setOnClickListener {
-            bookmarksAdapter.loadMoreBookmarks()
-            showToast(msgId = R.string.text_loaded_successfully)
-        }
-    }
+		buttonLoadMoreBookmarks.setOnClickListener {
+			bookmarksAdapter.loadMoreBookmarks()
+			showToast(msgId = R.string.text_loaded_successfully)
+		}
+	}
 
-    private fun deleteAllBookmarks() {
-        MsgDialogUtils.getMessageDialog(
-            baseActivityInf = safeBookmarksActivityRef,
-            isTitleVisible = true,
-            titleTextViewCustomize =
-                { it.setText(R.string.title_are_you_sure_about_this) },
-            messageTxt = getText(R.string.text_delete_bookmarks_confirmation),
-            isNegativeButtonVisible = false,
-            positiveButtonTextCustomize = {
-                it.setText(R.string.title_clear_now)
-                it.setLeftSideDrawable(R.drawable.ic_button_clear)
-            }
-        )?.apply {
-            setOnClickForPositiveButton {
-                close()
-                aioBookmark.getBookmarkLibrary().clear()
-                aioBookmark.updateInStorage()
-                bookmarksAdapter.resetBookmarkAdapter()
-            }
-        }?.show()
-    }
+	private fun deleteAllBookmarks() {
+		MsgDialogUtils.getMessageDialog(
+			baseActivityInf = safeBookmarksActivityRef,
+			isTitleVisible = true,
+			titleTextViewCustomize =
+				{ it.setText(R.string.title_are_you_sure_about_this) },
+			messageTxt = getText(R.string.text_delete_bookmarks_confirmation),
+			isNegativeButtonVisible = false,
+			positiveButtonTextCustomize = {
+				it.setText(R.string.title_clear_now)
+				it.setLeftSideDrawable(R.drawable.ic_button_clear)
+			}
+		)?.apply {
+			setOnClickForPositiveButton {
+				close()
+				aioBookmark.getBookmarkLibrary().clear()
+				aioBookmark.updateInStorage()
+				bookmarksAdapter.resetBookmarkAdapter()
+			}
+		}?.show()
+	}
 
-    private fun registerToAIOTimer() {
-        safeBookmarksActivityRef?.let { aioTimer.register(it) }
-    }
+	private fun registerToAIOTimer() {
+		safeBookmarksActivityRef?.let { aioTimer.register(it) }
+	}
 
-    private fun unregisterToAIOTimer() {
-        safeBookmarksActivityRef?.let { aioTimer.unregister(it) }
-    }
+	private fun unregisterToAIOTimer() {
+		safeBookmarksActivityRef?.let { aioTimer.unregister(it) }
+	}
 
-    fun updateBookmarkListAdapter() {
-        bookmarksAdapter.resetBookmarkAdapter()
-        bookmarksAdapter.loadMoreBookmarks()
-    }
+	fun updateBookmarkListAdapter() {
+		bookmarksAdapter.resetBookmarkAdapter()
+		bookmarksAdapter.loadMoreBookmarks()
+	}
 
-    private fun updateLoadMoreButtonVisibility() {
-        val bookmarkSize = aioBookmark.getBookmarkLibrary().size
-        if (bookmarksAdapter.count >= bookmarkSize) hideView(buttonLoadMoreBookmarks, true)
-        else showView(buttonLoadMoreBookmarks, true)
+	private fun updateLoadMoreButtonVisibility() {
+		val bookmarkSize = aioBookmark.getBookmarkLibrary().size
+		if (bookmarksAdapter.count >= bookmarkSize) hideView(buttonLoadMoreBookmarks, true)
+		else showView(buttonLoadMoreBookmarks, true)
 
-        if (bookmarksAdapter.isEmpty) showView(emptyBookmarksIndicator, true)
-        else {
-            hideView(emptyBookmarksIndicator, true).let {
-                CommonTimeUtils.delay(500, object : OnTaskFinishListener {
-                    override fun afterDelay() {
-                        showView(bookmarkListView, true)
-                    }
-                })
-            }
-        }
-    }
+		if (bookmarksAdapter.isEmpty) showView(emptyBookmarksIndicator, true)
+		else {
+			hideView(emptyBookmarksIndicator, true).let {
+				CommonTimeUtils.delay(500, object : OnTaskFinishListener {
+					override fun afterDelay() {
+						showView(bookmarkListView, true)
+					}
+				})
+			}
+		}
+	}
 
-    private fun openBookmarkInBrowser(bookmarkModel: BookmarkModel) {
-        sendResultBackToBrowserFragment(bookmarkModel.bookmarkUrl)
-        onBackPressActivity()
-    }
+	private fun openBookmarkInBrowser(bookmarkModel: BookmarkModel) {
+		sendResultBackToBrowserFragment(bookmarkModel.bookmarkUrl)
+		onBackPressActivity()
+	}
 
-    fun sendResultBackToBrowserFragment(result: String) {
-        setResult(RESULT_OK, Intent().apply {
-            putExtra(ACTIVITY_RESULT_KEY, result)
-        }).let { onBackPressActivity() }
-    }
+	fun sendResultBackToBrowserFragment(result: String) {
+		setResult(RESULT_OK, Intent().apply {
+			putExtra(ACTIVITY_RESULT_KEY, result)
+		}).let { onBackPressActivity() }
+	}
 }
--- app/src/main/java/app/ui/main/fragments/browser/activities/HistoryActivity.kt
+++ app/src/main/java/app/ui/main/fragments/browser/activities/HistoryActivity.kt
@@ -23,168 +23,169 @@ import lib.ui.builders.ToastView.Companion.showToast
 import java.lang.ref.WeakReference
 
 class HistoryActivity : BaseActivity(),
-    AIOTimerListener, OnHistoryItemClick, OnHistoryItemLongClick {
+	AIOTimerListener, OnHistoryItemClick, OnHistoryItemLongClick {
 
-    private val safeSelfReference = WeakReference(this)
-    private val safeHistoryActivityRef = safeSelfReference.get()
+	private val safeSelfReference = WeakReference(this)
+	private val safeHistoryActivityRef = safeSelfReference.get()
 
-    private lateinit var emptyHistoryIndicator: View
-    private lateinit var historyList: ListView
-    private lateinit var buttonLoadMoreHistory: View
+	private lateinit var emptyHistoryIndicator: View
+	private lateinit var historyList: ListView
+	private lateinit var buttonLoadMoreHistory: View
 
-    private var historyOptionPopup: HistoryOptionPopup? = null
+	private var historyOptionPopup: HistoryOptionPopup? = null
 
-    private val arg = safeHistoryActivityRef
-    private val historyAdapter by lazy { HistoryAdapter(arg, arg, arg) }
+	private val arg = safeHistoryActivityRef
+	private lateinit var historyAdapter: HistoryAdapter
 
-    override fun onRenderingLayout(): Int {
-        return R.layout.activity_browser_history_1
-    }
+	override fun onRenderingLayout(): Int {
+		return R.layout.activity_browser_history_1
+	}
 
-    override fun onAfterLayoutRender() {
-        initializeViews()
-        initializeViewsOnClickEvents()
-    }
+	override fun onAfterLayoutRender() {
+		initializeViews()
+		initializeViewsOnClickEvents()
+	}
 
-    override fun onResumeActivity() {
-        super.onResumeActivity()
-        registerToAIOTimer()
-        updateHistoryListAdapter()
-    }
+	override fun onResumeActivity() {
+		super.onResumeActivity()
+		registerToAIOTimer()
+		updateHistoryListAdapter()
+	}
 
-    override fun onPauseActivity() {
-        super.onPauseActivity()
-        unregisterToAIOTimer()
-    }
+	override fun onPauseActivity() {
+		super.onPauseActivity()
+		unregisterToAIOTimer()
+	}
 
-    override fun onBackPressActivity() {
-        closeActivityWithFadeAnimation(false)
-    }
+	override fun onBackPressActivity() {
+		closeActivityWithFadeAnimation(false)
+	}
 
-    override fun onDestroy() {
-        unregisterToAIOTimer()
-        historyList.adapter = null
-        historyOptionPopup?.close()
-        historyOptionPopup = null
-        super.onDestroy()
-    }
+	override fun onDestroy() {
+		unregisterToAIOTimer()
+		historyList.adapter = null
+		historyOptionPopup?.close()
+		historyOptionPopup = null
+		super.onDestroy()
+	}
 
-    override fun clearWeakActivityReference() {
-        safeSelfReference.clear()
-        super.clearWeakActivityReference()
-    }
+	override fun clearWeakActivityReference() {
+		safeSelfReference.clear()
+		super.clearWeakActivityReference()
+	}
 
-    override fun onAIOTimerTick(loopCount: Double) {
-        updateLoadMoreButtonVisibility()
-    }
+	override fun onAIOTimerTick(loopCount: Double) {
+		updateLoadMoreButtonVisibility()
+	}
 
-    override fun onHistoryItemClick(historyModel: HistoryModel) {
-        openHistoryInBrowser(historyModel)
-    }
+	override fun onHistoryItemClick(historyModel: HistoryModel) {
+		openHistoryInBrowser(historyModel)
+	}
 
-    override fun onHistoryItemLongClick(
-        historyModel: HistoryModel,
-        position: Int, listView: View
-    ) {
-        safeHistoryActivityRef?.let { safeActivityRef ->
-            try {
-                historyOptionPopup = HistoryOptionPopup(
-                    historyActivity = safeActivityRef,
-                    historyModel = historyModel,
-                    listView = listView
-                )
-                historyOptionPopup?.show()
-            } catch (error: Exception) {
-                error.printStackTrace()
-                showToast(msgId = R.string.text_something_went_wrong)
-            }
-        }
-    }
+	override fun onHistoryItemLongClick(
+		historyModel: HistoryModel,
+		position: Int, listView: View
+	) {
+		safeHistoryActivityRef?.let { safeActivityRef ->
+			try {
+				historyOptionPopup = HistoryOptionPopup(
+					historyActivity = safeActivityRef,
+					historyModel = historyModel,
+					listView = listView
+				)
+				historyOptionPopup?.show()
+			} catch (error: Exception) {
+				error.printStackTrace()
+				showToast(msgId = R.string.text_something_went_wrong)
+			}
+		}
+	}
 
-    private fun initializeViews() {
-        safeHistoryActivityRef?.let {
-            emptyHistoryIndicator = findViewById(R.id.empty_history_indicator)
-            historyList = findViewById(R.id.list_history)
-            buttonLoadMoreHistory = findViewById(R.id.btn_load_more_history)
+	private fun initializeViews() {
+		safeHistoryActivityRef?.let {
+			emptyHistoryIndicator = findViewById(R.id.empty_history_indicator)
+			historyList = findViewById(R.id.list_history)
+			buttonLoadMoreHistory = findViewById(R.id.btn_load_more_history)
 
-            historyList.adapter = historyAdapter
-            historyList.visibility = GONE
+			historyAdapter = HistoryAdapter(it, it, it)
+			historyList.adapter = historyAdapter
+			historyList.visibility = GONE
 
-            emptyHistoryIndicator.visibility = GONE
-            buttonLoadMoreHistory.visibility = GONE
-            updateLoadMoreButtonVisibility()
-        }
-    }
+			emptyHistoryIndicator.visibility = GONE
+			buttonLoadMoreHistory.visibility = GONE
+			updateLoadMoreButtonVisibility()
+		}
+	}
 
-    private fun initializeViewsOnClickEvents() {
-        findViewById<View>(R.id.btn_left_actionbar)
-            .setOnClickListener { onBackPressActivity() }
+	private fun initializeViewsOnClickEvents() {
+		findViewById<View>(R.id.btn_left_actionbar)
+			.setOnClickListener { onBackPressActivity() }
 
-        findViewById<View>(R.id.btn_right_actionbar)
-            .setOnClickListener { deleteAllHistory() }
+		findViewById<View>(R.id.btn_right_actionbar)
+			.setOnClickListener { deleteAllHistory() }
 
-        buttonLoadMoreHistory.setOnClickListener {
-            historyAdapter.loadMoreHistory()
-            showToast(msgId = R.string.text_loaded_successfully)
-        }
-    }
+		buttonLoadMoreHistory.setOnClickListener {
+			historyAdapter.loadMoreHistory()
+			showToast(msgId = R.string.text_loaded_successfully)
+		}
+	}
 
-    private fun deleteAllHistory() {
-        MsgDialogUtils.getMessageDialog(
-            baseActivityInf = safeHistoryActivityRef,
-            isTitleVisible = true,
-            titleTextViewCustomize = { it.setText(R.string.title_are_you_sure_about_this) },
-            messageTxt = getText(R.string.text_delete_browsing_history_confirmation),
-            isNegativeButtonVisible = false,
-            positiveButtonTextCustomize = {
-                it.setText(R.string.title_clear_now)
-                it.setLeftSideDrawable(R.drawable.ic_button_clear)
-            })?.apply {
-            setOnClickForPositiveButton {
-                close()
-                aioHistory.getHistoryLibrary().clear()
-                aioHistory.updateInStorage()
-                historyAdapter.resetHistoryAdapter()
-            }
-        }?.show()
-    }
+	private fun deleteAllHistory() {
+		MsgDialogUtils.getMessageDialog(
+			baseActivityInf = safeHistoryActivityRef,
+			isTitleVisible = true,
+			titleTextViewCustomize = { it.setText(R.string.title_are_you_sure_about_this) },
+			messageTxt = getText(R.string.text_delete_browsing_history_confirmation),
+			isNegativeButtonVisible = false,
+			positiveButtonTextCustomize = {
+				it.setText(R.string.title_clear_now)
+				it.setLeftSideDrawable(R.drawable.ic_button_clear)
+			})?.apply {
+			setOnClickForPositiveButton {
+				close()
+				aioHistory.getHistoryLibrary().clear()
+				aioHistory.updateInStorage()
+				historyAdapter.resetHistoryAdapter()
+			}
+		}?.show()
+	}
 
-    private fun registerToAIOTimer() {
-        safeHistoryActivityRef?.let { aioTimer.register(it) }
-    }
+	private fun registerToAIOTimer() {
+		safeHistoryActivityRef?.let { aioTimer.register(it) }
+	}
 
-    private fun unregisterToAIOTimer() {
-        safeHistoryActivityRef?.let { aioTimer.unregister(it) }
-    }
+	private fun unregisterToAIOTimer() {
+		safeHistoryActivityRef?.let { aioTimer.unregister(it) }
+	}
 
-    fun updateHistoryListAdapter() {
-        historyAdapter.resetHistoryAdapter()
-        historyAdapter.loadMoreHistory()
-    }
+	fun updateHistoryListAdapter() {
+		historyAdapter.resetHistoryAdapter()
+		historyAdapter.loadMoreHistory()
+	}
 
-    private fun updateLoadMoreButtonVisibility() {
-        val historySize = aioHistory.getHistoryLibrary().size
-        if (historyAdapter.count >= historySize) hideView(buttonLoadMoreHistory, true)
-        else showView(buttonLoadMoreHistory, true)
+	private fun updateLoadMoreButtonVisibility() {
+		val historySize = aioHistory.getHistoryLibrary().size
+		if (historyAdapter.count >= historySize) hideView(buttonLoadMoreHistory, true)
+		else showView(buttonLoadMoreHistory, true)
 
-        if (historyAdapter.isEmpty) showView(emptyHistoryIndicator, true)
-        else hideView(emptyHistoryIndicator, true).let {
-            CommonTimeUtils.delay(500, object : OnTaskFinishListener {
-                override fun afterDelay() {
-                    showView(historyList, true)
-                }
-            })
-        }
-    }
+		if (historyAdapter.isEmpty) showView(emptyHistoryIndicator, true)
+		else hideView(emptyHistoryIndicator, true).let {
+			CommonTimeUtils.delay(500, object : OnTaskFinishListener {
+				override fun afterDelay() {
+					showView(historyList, true)
+				}
+			})
+		}
+	}
 
-    private fun openHistoryInBrowser(historyModel: HistoryModel) {
-        sendResultBackToBrowserFragment(historyModel.historyUrl)
-        onBackPressActivity()
-    }
+	private fun openHistoryInBrowser(historyModel: HistoryModel) {
+		sendResultBackToBrowserFragment(historyModel.historyUrl)
+		onBackPressActivity()
+	}
 
-    fun sendResultBackToBrowserFragment(result: String) {
-        setResult(RESULT_OK, Intent().apply {
-            putExtra(ACTIVITY_RESULT_KEY, result)
-        }).let { onBackPressActivity() }
-    }
+	fun sendResultBackToBrowserFragment(result: String) {
+		setResult(RESULT_OK, Intent().apply {
+			putExtra(ACTIVITY_RESULT_KEY, result)
+		}).let { onBackPressActivity() }
+	}
 }
